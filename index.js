94645ec01f1abb161f4b9a1223c51ca745f91331
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-09 00:32:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) 
+function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you";
-} else if {
-  (string) === (string.toUpperCase())
+} else if 
+  (string === string.toUpperCase())
     return 'YES INDEED';
-    } else { (string) === 'I love you, Grandma.'
+  } else { (string === 'I love you, Grandma.')
     return 'I love you, too.'
 }
